f85d867892cd2faaf2212da49866104e6043510a
viachaslau <dEment0rc0mpany@gmail.com>
2024-02-13 17:20:58 +0300

    add edit, deleteById, deleteAll to OrderController

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -13,7 +13,7 @@ export class OrderController {
   constructor(private orderService: OrderService) {
   }
 
-  @ApiOperation({summary: 'Получение заказа по ID'})
+  @ApiOperation({ summary: 'Получение заказа по ID' })
   @ApiParam({
     name: 'id',
     required: true,
@@ -26,12 +26,12 @@ export class OrderController {
   @Get('get/:id')
   async getById(
     @Req() req: UserRequest,
-    @Param('id', new ParseIntPipe()) id: number
+    @Param('id', new ParseIntPipe()) id: number,
   ) {
     return this.orderService.getById(req.user.company, id);
   }
 
-  @ApiOperation({summary: 'Получение всех заказов'})
+  @ApiOperation({ summary: 'Получение всех заказов' })
   @ApiBearerAuth('JWT-auth')
   @Roles(Role.ADMIN, Role.MANAGER)
   @UseGuards(RolesGuard)
@@ -42,63 +42,63 @@ export class OrderController {
     return this.orderService.getAll(req.user.company);
   }
 
-  @ApiOperation({summary: 'Создание заказа'})
+  @ApiOperation({ summary: 'Создание заказа' })
   @ApiBearerAuth('JWT-auth')
   @Roles(Role.ADMIN, Role.MANAGER)
   @UseGuards(RolesGuard)
   @Post('create')
   async create(
     @Req() req: UserRequest,
-    @Body() createDto: CreateOrderDto
+    @Body() createDto: CreateOrderDto,
   ) {
     return this.orderService.create(createDto, req.user.company, req.user.id);
   }
 
-  // @ApiOperation({summary: 'Изменение заказа'})
-  // @ApiParam({
-  //   name: 'id',
-  //   required: true,
-  //   example: 1,
-  //   description: 'ID заказа',
-  // })
-  // @ApiBearerAuth('JWT-auth')
-  // @Roles(Role.ADMIN, Role.MANAGER)
-  // @UseGuards(RolesGuard)
-  // @Put('edit/:id')
-  // async edit(
-  //   @Req() req: UserRequest,
-  //   @Body() editDto: EditOrderDto,
-  //   @Param('id', new ParseIntPipe()) id: number
-  // ) {
-  //
-  // }
-  //
-  // @ApiOperation({summary: 'Удаление заказа по ID'})
-  // @ApiParam({
-  //   name: 'id',
-  //   required: true,
-  //   example: 1,
-  //   description: 'ID заказа',
-  // })
-  // @ApiBearerAuth('JWT-auth')
-  // @Roles(Role.ADMIN, Role.MANAGER)
-  // @UseGuards(RolesGuard)
-  // @Delete('delete/:id')
-  // async deleteById(
-  //   @Req() req: UserRequest,
-  //   @Param('id', new ParseIntPipe()) id: number
-  // ) {
-  //
-  // }
-  //
-  // @ApiOperation({summary: 'Удаление всех заказов'})
-  // @ApiBearerAuth('JWT-auth')
-  // @Roles(Role.ADMIN, Role.MANAGER)
-  // @UseGuards(RolesGuard)
-  // @Delete('deleteAll')
-  // async deleteAll(
-  //   @Req() req: UserRequest,
-  // ) {
-  //
-  // }
+  @ApiOperation({ summary: 'Изменение заказа' })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    example: 1,
+    description: 'ID заказа',
+  })
+  @ApiBearerAuth('JWT-auth')
+  @Roles(Role.ADMIN, Role.MANAGER)
+  @UseGuards(RolesGuard)
+  @Put('edit/:id')
+  async edit(
+    @Req() req: UserRequest,
+    @Body() editDto: EditOrderDto,
+    @Param('id', new ParseIntPipe()) id: number,
+  ) {
+    return this.orderService.edit(editDto, req.user.company, id);
+  }
+
+  @ApiOperation({ summary: 'Удаление заказа по ID' })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    example: 1,
+    description: 'ID заказа',
+  })
+  @ApiBearerAuth('JWT-auth')
+  @Roles(Role.ADMIN, Role.MANAGER)
+  @UseGuards(RolesGuard)
+  @Delete('delete/:id')
+  async deleteById(
+    @Req() req: UserRequest,
+    @Param('id', new ParseIntPipe()) id: number,
+  ) {
+    return this.orderService.deleteById(req.user.company, id);
+  }
+
+  @ApiOperation({ summary: 'Удаление всех заказов' })
+  @ApiBearerAuth('JWT-auth')
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @Delete('deleteAll')
+  async deleteAll(
+    @Req() req: UserRequest,
+  ) {
+    return this.orderService.deleteAll(req.user.company);
+  }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { EntityManager, Repository } from 'typeorm';
 import { CompanyService } from '../company/company.service';
-import { CreateOrderDto } from './dtos';
+import { CreateOrderDto, EditOrderDto } from './dtos';
 import { EXCEPTION_MESSAGE } from '../constants';
 import { UserService } from '../user/user.service';
 import { PaymentMethodService } from '../paymentMethod/paymentMethod.service';
@@ -11,6 +11,8 @@ import { StatusService } from '../status/status.service';
 import { SolutionService } from '../solution/solution.service';
 import { IndividualClientService } from '../client/individual-client/individual-client.service';
 import { CorporateClientService } from '../client/corporate-client/corporate-client.service';
+import { IndividualClient } from '../client/individual-client/entity/individualClient.entity';
+import { CorporateClient } from '../client/corporate-client/entity/corporateClient.entity';
 
 @Injectable()
 export class OrderService {
@@ -23,7 +25,7 @@ export class OrderService {
     private readonly statusService: StatusService,
     private readonly solutionService: SolutionService,
     private readonly individualClientService: IndividualClientService,
-    private readonly corporateClientService: CorporateClientService
+    private readonly corporateClientService: CorporateClientService,
   ) {
   }
 
@@ -38,8 +40,8 @@ export class OrderService {
       .leftJoinAndSelect('order.company', 'company')
       .leftJoinAndSelect('order.status', 'status')
       .leftJoinAndSelect('order.user', 'client')
-      .where('order.id = :orderId', {orderId})
-      .andWhere('company.id = :companyId', {companyId})
+      .where('order.id = :orderId', { orderId })
+      .andWhere('company.id = :companyId', { companyId })
       .getOne();
   }
 
@@ -54,7 +56,7 @@ export class OrderService {
       .leftJoinAndSelect('order.company', 'company')
       .leftJoinAndSelect('order.status', 'status')
       .leftJoinAndSelect('order.user', 'client')
-      .where('company.id = :companyId', {companyId})
+      .where('company.id = :companyId', { companyId })
       .getManyAndCount();
   }
 
@@ -129,11 +131,147 @@ export class OrderService {
       stuff: stuff,
       paymentMethod: paymentMethod,
       ...(dto.clientType === 'individualClient'
-        ? {individualClient: client}
-        : {corporateClient: client}),
+        ? { individualClient: client }
+        : { corporateClient: client }),
       company: company,
       status: status,
-      user: user
+      user: user,
     });
+  }
+
+  async edit(dto: EditOrderDto, companyId: number, orderId: number) {
+    const company = await this.companyService.getCompanyWithEntityId(companyId, orderId, 'order');
+    if (!company) {
+      throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND_BY_ID);
+    }
+
+    const order = await this.orderRepository.findOne({
+      where: {
+        id: orderId,
+      },
+    });
+
+    if (!order) {
+      throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND_BY_ID);
+    }
+
+    if (dto.clientType && dto.clientId) {
+      let client = {};
+      if (dto.clientType === 'individualClient') {
+        client = await this.individualClientService.getById(dto.clientId, companyId);
+      } else {
+        client = await this.corporateClientService.getById(dto.clientId, companyId);
+      }
+
+      if (!client) {
+        throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND_BY_ID);
+      }
+
+      if (dto.clientType === 'individualClient') {
+        order.corporateClient = null;
+        order.individualClient = client as IndividualClient;
+      } else {
+        order.individualClient = null;
+        order.corporateClient = client as CorporateClient;
+      }
+      order.clientType = dto.clientType;
+    }
+
+    if (dto.orderDate) {
+      order.orderDate = dto.orderDate;
+    }
+
+    if (dto.address) {
+      order.address = dto.address;
+    }
+
+    if (dto.price) {
+      order.price = dto.price;
+    }
+
+    if (dto.comment) {
+      order.comment = dto.comment;
+    }
+
+    if (dto.paymentMethodId) {
+      const paymentMethod = await this.paymentMethodService.getById(dto.paymentMethodId, companyId);
+      if (!paymentMethod) {
+        throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND_BY_ID);
+      }
+
+      order.paymentMethod = paymentMethod;
+    }
+
+    if (dto.statusId) {
+      const status = await this.statusService.getById(dto.statusId, companyId);
+      if (!status) {
+        throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND_BY_ID);
+      }
+
+      order.status = status;
+    }
+
+    if (dto.solutionsIds.length) {
+      const solutions = await Promise.all(dto.solutionsIds.map(async (solutionId) => {
+        const solution = await this.solutionService.getById(solutionId, companyId);
+        if (!solution) {
+          throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND_BY_ID);
+        }
+        return solution;
+      }));
+
+      if (!solutions.length) {
+        throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND_BY_ID);
+      }
+
+      order.solutions = solutions;
+    }
+
+    if (dto.stuffIds.length) {
+      const stuff = await Promise.all(dto.solutionsIds.map(async (userId) => {
+        const user = await this.userService.getUserById(userId);
+        if (!user) {
+          throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND_BY_ID);
+        }
+        return user;
+      }));
+
+      if (!stuff.length) {
+        throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND_BY_ID);
+      }
+
+      order.stuff = stuff;
+    }
+
+    return this.orderRepository.save(order);
+  }
+
+  async deleteById(companyId: number, orderId: number) {
+    const company = await this.companyService.getCompanyWithEntityId(companyId, orderId, 'order');
+    if (!company) {
+      throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND);
+    }
+
+    const order = company.orders.find(order => order.id === orderId);
+    if (!order) {
+      throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND_BY_ID);
+    }
+
+    await this.orderRepository.remove(order as any);
+
+    return true;
+  }
+
+  async deleteAll(companyId: number) {
+    const company = await this.companyService.getCompanyWithEntity(companyId, 'order');
+    if (!company) {
+      throw new BadRequestException(EXCEPTION_MESSAGE.BAD_REQUEST_EXCEPTION.NOT_FOUND);
+    }
+
+    company.orders = [];
+
+    await this.entityManager.save(company);
+
+    return true;
   }
 }
